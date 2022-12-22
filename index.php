58b49fd3881110f8560ab0185cf631b2c1874213
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2022-12-21 23:25:29 -0500

    barra de navegacion y tarjetas principales
    Plantillas de tarjetas y barra de navegacio cargadas.

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description:" content="Grupo empresarial enfocado en soluciones logisticas nacionales e internacionales.">
+    <meta name="author" content="mashacorp-2022">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="DC.Languague" schemet="rfc1766" content="spanish">
+    <meta property="og:locale" content="es_EC">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Grupo Revilla">
+    <meta property="og:description" content="Logistica integra">
+    <meta property="og:url" content="https://gruporevilla.world/">
+    <meta property="og:site_name" content="Grupo revilla">
+    <link rel="shortcut icon" href="img/logo_gr.png">
+    <link rel="stylesheet" href="styles/styles.css">
+    <!-- integracion de bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Grupo Revilla</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="left">
+            <h1>Grupo Revilla</h1>
+        </div>
+        <div class="right">
+            <input type="checkbox" id="check">
+            <label for="check" class="checkBtn">
+                <i class="fa fa-bars"></i>
+            </label>
+            <ul class="list">
+                <li>
+                    <a href="#">Quienes Somos</a>
+                </li>
+                <li>
+                    <a href="#">Contactos</a>
+                </li>
+                <li>
+                    <a href="#">Trabaja con nosotros</a>
+                </li>
+                <li>
+                    <a href="#">Ingresar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="card-main-container">
+        <div class="card-main">
+            <div class="front-card">
+                <img src="./user.png" alt="">
+                <h2>Raju</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore repellat quis.</p>
+            </div>
+            <div class="back-card">
+                <h2 class="back-light">Thank You!</h2>
+                <p class="back-light"> Social Media Management </p>
+                <p class="btn"><a class="back-light" href="#">Know More</a></p>
+            </div>
+        </div>
+        <div class="card-main">
+            <div class="front-card">
+                <img src="./user.png" alt="">
+                <h2>Raju</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore repellat quis.</p>
+            </div>
+            <div class="back-card">
+                <h2 class="back-light">Thank You!</h2>
+                <p class="back-light"> Social Media Management </p>
+                <p class="btn"><a class="back-light" href="#">Know More</a></p>
+            </div>
+        </div>
+        <div class="card-main">
+            <div class="front-card">
+                <img src="./user.png" alt="">
+                <h2>Raju</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore repellat quis.</p>
+            </div>
+            <div class="back-card">
+                <h2 class="back-light">Thank You!</h2>
+                <p class="back-light"> Social Media Management </p>
+                <p class="btn"><a class="back-light" href="#">Know More</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
