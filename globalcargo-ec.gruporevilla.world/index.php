9f3de391bc074d6f241f2345718a731a51dbeedf
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2023-01-03 23:46:47 -0500

    prog en imac
    Creacion de estructura de carpetas de paginas puntuales

---
+++ globalcargo-ec.gruporevilla.world/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description:" content="Grupo empresarial enfocado en soluciones logisticas nacionales e internacionales.">
+    <meta name="author" content="mashacorp-2022">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="DC.Languague" schemet="rfc1766" content="spanish">
+    <meta property="og:locale" content="es_EC">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Grupo Revilla">
+    <meta property="og:description" content="Logistica integra">
+    <meta property="og:url" content="https://gruporevilla.world/">
+    <meta property="og:site_name" content="Grupo revilla">
+    <link rel="shortcut icon" href="img/logo_gr.png">
+    <link rel="stylesheet" href="styles/styles.css">
+    <!-- integracion de bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <title>Grupo Revilla</title>
+</head>
+<body class="principal"> 
+
+<footer class="footer">
+            <div class="fotter_container">
+                <div class="footer_top">
+                    <div class="company_info">
+                        <h2 class="company_logo"></h2>
+                        <p class="company_description"></p>
+                        <ul class="footer_list">
+                            <li class="footer_list-item">
+                                <a href="#" class="footer_list-link">
+                                    <i class="ri-facebook-circle-line"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <span class="copyrigth">...</span>
+                    </div>
+                    <div>
+                        <h6 class="footer_title"></h6>
+                        <ul class="footer_list">
+                            <li class="footer_list-item">
+                                <a href="#" class="footer_list-link"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,14 +1,27 @@
         <footer class="footer">
-        <!-- derechos de uso para la el wb site -->
-            <div>
-                <p class="derechos">Todos los derechos reservados GRUPO REVILLA</p>
-            </div>
-        <!-- creadores y empresa que desarrolla el software -->
-            <div>
-                <p class="derechos">MashaWeb Corp 
-                    <span class="dis">designed by</span>
-                    <span class="autor">Pablo Revilla</span>
-                </p>
+            <div class="fotter_container">
+                <div class="footer_top">
+                    <div class="company_info">
+                        <h2 class="company_logo"></h2>
+                        <p class="company_description"></p>
+                        <ul class="footer_list">
+                            <li class="footer_list-item">
+                                <a href="#" class="footer_list-link">
+                                    <i class="ri-facebook-circle-line"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <span class="copyrigth">...</span>
+                    </div>
+                    <div>
+                        <h6 class="footer_title"></h6>
+                        <ul class="footer_list">
+                            <li class="footer_list-item">
+                                <a href="#" class="footer_list-link"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </footer>
     </body>
