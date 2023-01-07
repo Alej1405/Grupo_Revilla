144607271b52b007e417cfd0711a186e0c9c6055
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2023-01-06 20:44:59 -0500

    actualización de composer en las dos maquinas para poder trabajar el backend de forma conjunta y sin perder el hilo cuando se cambia de computador.

--- includes/funciones.php
+++ includes/funciones.php
@@ -3,7 +3,7 @@ define ('TEMPLATES_URL', __DIR__ . '/templates');
 define ('FUNCIONES_URL', __DIR__ . 'funciones.php');
 
 function incluirTemplate ($nombre) {
-    include TEMPLATES_URL . "/${nombre}.php";
+    include TEMPLATES_URL . "/$nombre.php";
 }
 
 function estaAutenticado () {
