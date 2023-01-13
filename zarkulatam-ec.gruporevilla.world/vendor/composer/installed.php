cd803df5ac049857c959d935faca8349579d553a
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2023-01-13 16:58:32 -0500

    creacion del autoload para zarku

---
+++ zarkulatam-ec.gruporevilla.world/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'macbook/zarkulatam-ec.gruporevilla.world',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '93b2b75e8b07486f5c9949d8375a0c46c3680c12',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'macbook/zarkulatam-ec.gruporevilla.world' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '93b2b75e8b07486f5c9949d8375a0c46c3680c12',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
