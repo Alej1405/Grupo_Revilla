e9cff5d22b202c63206e27a7994b27524dcffd22
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2023-03-13 18:21:05 -0500

    actualizar a imac

--- gc-box.gruporevilla.world/index.php
+++ gc-box.gruporevilla.world/index.php
@@ -4,21 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description:" content="Grupo empresarial enfocado en soluciones logisticas nacionales e internacionales.">
-    <meta name="author" content="mashacorp-2022">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description:" content="El mejor courier rapido y efectivo, aprendes a importar sin intermediarios.">
+    <meta name="author" content="mashacorp-2022">
     <meta name="DC.Languague" schemet="rfc1766" content="spanish">
     <meta property="og:locale" content="es_EC">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Grupo Revilla">
-    <meta property="og:description" content="Logistica integra">
-    <meta property="og:url" content="https://gruporevilla.world/">
-    <meta property="og:site_name" content="Grupo revilla">
-    <link rel="shortcut icon" href="img/logo_gr.png">
+    <meta property="og:title" content="GC_BOX">
+    <meta property="og:description" content="El mejor courier rapido y efectivo, aprendes a importar sin intermediarios.">
+    <meta property="og:url" content="https://gc-box.gruporevilla.world/">
+    <meta property="og:site_name" content="Gc-box Courier">
+    <link rel="shortcut icon" href="img/logo_gcbox.png">
     <link rel="stylesheet" href="styles/styles.css">
     <!-- api fontawsome -->
-    <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/36e9f60aba.js" crossorigin="anonymous"></script>
     <!-- integracion de bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
@@ -28,38 +28,29 @@
 </head>
 
 <body class="principal">
+    <div class="background"></div>
     <nav class="navbar">
-        <div class="left">
-
-            <h1><img src="./img/logo_gr.png" class="logo_navbar" alt="logo grupo revilla">upo Revilla</h1>
-        </div>
-        <div class="right">
-            <input type="checkbox" id="check">
-            <label for="check" class="checkBtn">
-                <i class="fa fa-bars"></i>
-            </label>
-            <ul class="list">
-                <li>
-                    <a href="#">Quienes Somos</a>
-                </li>
-                <li>
-                    <a href="#">Contactos</a>
-                </li>
-                <li>
-                    <a href="#">Trabaja con nosotros</a>
-                </li>
-                <li>
-                    <a href="src/login.php">Ingresar</a>
-                </li>
-            </ul>
-        </div>
+      <img class="navbar-logo" src="./assets/logo.png" />
+      <input type="text" placeholder="Search" class="navbar-search" />
     </nav>
-    <section class="card-main-container">
+    <button class="navbar-burger" onclick="toggleMenu()"></button>
+    <nav class="menu">
+      <a href="#" style="animation-delay: 0.1s">Home</a>
+      <a href="#" style="animation-delay: 0.2s">About</a>
+      <a href="#" style="animation-delay: 0.3s">Services</a>
+      <a href="#" style="animation-delay: 0.4s">Products</a>
+      <a href="#" style="animation-delay: 0.5s">Contact</a>
+    </nav>
 
         <footer class="footer">
 
         </footer>
 
+    <script type="text/javascript">
+      const toggleMenu = () => {
+        document.body.classList.toggle("open");
+      };
+    </script>
 </body>
 
 </html>
