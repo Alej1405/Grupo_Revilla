cbcf428871842045dedf03327861678064f9fb37
Alej1405 <103963976+Alej1405@users.noreply.github.com>
2023-03-13 11:24:41 -0500

    instalacion de fontawsome

--- gc-box.gruporevilla.world/index.php
+++ gc-box.gruporevilla.world/index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description:" content="Grupo empresarial enfocado en soluciones logisticas nacionales e internacionales.">
@@ -16,18 +17,21 @@
     <meta property="og:site_name" content="Grupo revilla">
     <link rel="shortcut icon" href="img/logo_gr.png">
     <link rel="stylesheet" href="styles/styles.css">
+    <!-- api fontawsome -->
+    <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <!-- integracion de bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <title>Grupo Revilla</title>
 </head>
-<body class="principal"> 
+
+<body class="principal">
     <nav class="navbar">
         <div class="left">
-            
-            <h1><img src="./img/logo_gr.png"  class="logo_navbar" alt="logo grupo revilla">upo Revilla</h1>
+
+            <h1><img src="./img/logo_gr.png" class="logo_navbar" alt="logo grupo revilla">upo Revilla</h1>
         </div>
         <div class="right">
             <input type="checkbox" id="check">
@@ -51,10 +55,11 @@
         </div>
     </nav>
     <section class="card-main-container">
-        
-    <footer class="footer">
-            
+
+        <footer class="footer">
+
         </footer>
 
 </body>
+
 </html>
